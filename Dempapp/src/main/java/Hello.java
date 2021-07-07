5cc52d97620ee058c892badc5eb6424a18a47ec2
bleedingwolf11 <56654525+bleedingwolf11@users.noreply.github.com>
2021-07-07 18:13:54 +0530

    commit all files.

---
+++ Dempapp/src/main/java/Hello.java
@@ -0,0 +1,13 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello");
+		System.out.print("wtmw");
+		System.out.print("nfu");
+		System.out.print("wtmw1");
+		System.out.print("nfu1");
+	}
+
+}
